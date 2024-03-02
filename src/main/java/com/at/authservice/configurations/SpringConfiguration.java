d00c7fd3bd4655db0b8e478e39e627227e7acb68
Akhilesh Trivedi <atakhileshtrivedi@gmail.com>
2024-03-02 16:04:51 +0530

    not JWT Token

--- src/main/java/com/at/authservice/configurations/SpringConfiguration.java
+++ src/main/java/com/at/authservice/configurations/SpringConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SpringConfiguration {
+    // It is a good practice to use a strong hashing algorithm like BCrypt to hash the passwords.
+    //It is not JWT Token specific, but it is a good practice to use it.
     @Bean
     public BCryptPasswordEncoder  passwordEncoder() {
         return new BCryptPasswordEncoder(16);
